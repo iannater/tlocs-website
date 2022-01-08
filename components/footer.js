47d330e516a7240b82a9ba4a309d0378222266a6
Fabian <acostahf4@gmail.com>
2022-01-08 12:47:18 -0600

    footer section, built with a grid layout

---
+++ components/footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "../styles/footer.module.css";
+import Image from "next/image";
+
+const footer = () => {
+  return (
+    <div>
+      <footer className={styles.footer}>
+        <div className={styles.items}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="">
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                width={72}
+                height={16}
+              />
+            </span>
+          </a>
+        </div>
+        <div className={styles.items}>
+          <h1 className={styles.title}>Hours:</h1>
+          <h1 className={styles.content}>Monday closed</h1>
+        </div>
+        <div className={styles.items}>
+          <h1 className={styles.title}>Our Home</h1>
+          <h1 className={styles.content}>5000 Burnet Rd, Austin, TX 78756</h1>
+        </div>
+        <div className={styles.items}>
+          <h1 className={styles.title}>Connect</h1>
+          <h1 className={styles.content}>
+            follow our journey on social channels
+          </h1>
+        </div>
+        <div className={styles.items}>
+          <h1 className={styles.title}>Nowhere to go but up</h1>
+          <h1 className={styles.content}>Privacy</h1>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default footer;
