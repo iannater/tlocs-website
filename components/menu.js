7c3e086a9b2e0a7564e2ff2c3342cfe0fb3e5ec2
Fabian <acostahf4@gmail.com>
2022-01-08 12:45:56 -0600

    menu section

---
+++ components/menu.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import styles from "../styles/Home.module.css";
+
+const menu = () => {
+  return (
+    <div>
+      <div className="text-center bg-yellowBg">
+        <h1 className="heading">MENU</h1>
+        <p className="sub">Hot Dogs, Burritos, Tacos y mas Estilo Sonora</p>
+      </div>
+    </div>
+  );
+};
+
+export default menu;
