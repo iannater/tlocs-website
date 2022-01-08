9d8efe9ca30208f39c2d816e712ed0c6de78ad47
Fabian <acostahf4@gmail.com>
2022-01-08 12:45:36 -0600

    navbar

---
+++ components/navbar.js
@@ -0,0 +1,31 @@
+const NavItem = (props) => (
+  <li>
+    <a
+      className="text-lg font-bold text-white px-2 py-1 hover:bg-red-900 rounded transition-colors duration-300"
+      href={props.href}
+    >
+      {props.text}
+    </a>
+  </li>
+);
+
+const navbar = () => (
+  <div>
+    <nav className="flex justify-between py-4 px-3 ">
+      <div className="flex items-center ">
+        <div className="inline-block h-6 w-6 rounded-full px-3 py-2 bg-gray-400" />
+        {/* <span className="ml-2"></span> */}
+      </div>
+      <div>
+        <ul className="flex space-x-2">
+          <NavItem href="/" text="Home" />
+          <NavItem href="/about" text="About" />
+          <NavItem href="/catering" text="Catering" />
+          <NavItem href="/contact" text="Contact" />
+        </ul>
+      </div>
+    </nav>
+  </div>
+);
+
+export default navbar;
