c2c6e5b3244b726994e0a90de9f1afe5de696fc6
Fabian <acostahf4@gmail.com>
2022-01-20 14:47:17 -0600

    brought in gsap animation library for the hotdog animation. useRefs are in place but not used.trigger needs improvements

--- components/tab.js
+++ components/tab.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 
-const tab = () => {
+const tab = (props) => {
   return (
-    <div className="bg-redBg p-5 rounded-lg flex justify-between  w-5/6">
+    <div
+      id="boxes"
+      className="bg-redBg p-5 rounded-lg flex justify-between  w-5/6"
+    >
       <div className="pl-10 text-white text-lg pr-10">
-        <h1>Diced Tomatoes</h1>
-        <div className="w-1/2 rounded-md h-1 bg-white ml-`"></div>
-        <p>Perfectly diced tomatoes sourced from the best farmers in Texas</p>
+        <h1>{props.title}</h1>
+        <div className="w-1/2 rounded-md h-1 bg-white ml-1"></div>
+        <p>{props.description}</p>
       </div>
       <div className="flex justify-center items-center pr-4">
         <Image src="/arrow.png" alt="arrow" width={30} height={50} />
