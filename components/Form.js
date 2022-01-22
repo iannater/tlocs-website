1f9868cc9a842634c76d5d373e2666fe9143613c
Fabian <acostahf4@gmail.com>
2022-01-22 11:39:11 -0600

    updated responsiveness

---
+++ components/Form.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div className="md:px-40">
+      <div className="bg-redBg md:rounded-lg shadow-lg grid lg:grid-cols-2">
+        <div className="p-10">
+          <div className="pb-4 text-center">
+            <h1 className="font-nmr text-4xl font-bold text-nmr-lightBrown">
+              Thinking of Catering?
+            </h1>
+          </div>
+          <form className="text-center">
+            <div className="w-full space-y-5 p-2 md:py-5 md:px-10">
+              <input
+                placeholder="name"
+                className="bg-nmr-lightBrown px-5 py-2 rounded-md w-full "
+              ></input>
+              <input
+                placeholder="email address"
+                className="bg-nmr-lightBrown px-5 py-2 rounded-md w-full "
+              ></input>
+              <input
+                placeholder="date of the event"
+                className="bg-nmr-lightBrown px-5 py-2 rounded-md w-full "
+              ></input>
+              <input
+                placeholder="number of people approx"
+                className="bg-nmr-lightBrown px-5 py-2 rounded-md w-full "
+              ></input>
+              <textarea
+                rows="3"
+                placeholder="details about the event (time, type of venue, private party or company event, etc.)"
+                className="bg-nmr-lightBrown px-5 py-2 rounded-md w-full shadow-md"
+              ></textarea>
+              <button className="bg-nmr-black text-nmr-lightBrown font-nmr px-10 py-3 text-xl rounded-md">
+                Send
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Form;
--- pages/catering.js
+++ pages/catering.js
@@ -1,106 +1,75 @@
 import React from "react";
 import Image from "next/image";
 import Layout2 from "../components/layoutSd";
+import Form from "../components/Form";
 
 const Catering = () => {
   return (
     <div>
       <div className="pt-20 md:py-20 bg-yellowBg">
-        <div className="md:bg-dog md:bg-contain md:bg-no-repeat md:bg-right md:-mr-56 md:h-96">
-          <div className="grid md:grid-cols-2 px-10 md:px-40 text-center md:text-left">
+        <div className="px-10 md:px-40 text-center md:text-left">
+          <div className="flex flex-col justify-center md:justify-start md:items-start md:max-w-md">
+            <h2 className="font-nmr font-bold text-2xl">Welcome to</h2>
+            <h1 className="text-[#D9453F] text-5xl font-bold font-nmr pb-2 ">
+              T-Loc&apos;s Catering
+            </h1>
+            <p className="font-semibold pb-5 md:pb-10  ">
+              Want to spread the love and show everyone what a T-Loc&apos;s hot
+              dog is all about? We now cater for any sized party and any event.
+            </p>
             <div>
-              <h2 className="font-nmr font-bold text-2xl">Welcome to</h2>
-              <h1 className="text-[#D9453F] text-4xl font-bold font-nmr pb-2">
-                T-Loc&apos;s Catering
-              </h1>
-              <p className="font-semibold pb-5 md:pb-10">
-                Want to spread the love and show everyone what a T-Loc&apos;s
-                hot dog is all about? We now cater for any sized party and any
-                event.
-              </p>
               <button className="bg-[#D9453F] text-[#F1E8DC] rounded-md px-5 py-2 text-lg font-bold font-nmr">
                 Book Now
               </button>
             </div>
-            <div></div>
           </div>
         </div>
 
-        <div className="grid md:grid-cols-3 text-center py-20 px-10 lg:px-40 gap-6 md:gap-0">
-          <div>
+        <div className="grid md:grid-cols-3 text-center py-20 px-10 md:px-40 gap-6 md:gap-4">
+          <div className="flex flex-col items-center">
             <div className="pb-4">
               <Image src="/phone.png" alt="phone icon" width={59} height={59} />
             </div>
-            <h1 className="font-nmr text-3xl text-nmr-redTxt">
-              Fresh Ingredients
-            </h1>
-            <h2 className="font-semibold">
-              We use only the best ingrdients, making sure nothing is ever
-              frozen and served perfect to you.
-            </h2>
-          </div>
-          <div>
-            <div className="pb-4">
-              <Image src="/phone.png" alt="phone icon" width={59} height={59} />
-            </div>
-            <h1 className="font-nmr text-3xl text-nmr-redTxt">
-              Fresh Ingredients
-            </h1>
-            <h2 className="font-semibold">
-              We use only the best ingrdients, making sure nothing is ever
-              frozen and served perfect to you.
-            </h2>
-          </div>
-          <div>
-            <div className="pb-4">
-              <Image src="/phone.png" alt="phone icon" width={59} height={59} />
-            </div>
-            <h1 className="font-nmr text-3xl text-nmr-redTxt">
-              Fresh Ingredients
-            </h1>
-            <h2 className="font-semibold">
-              We use only the best ingrdients, making sure nothing is ever
-              frozen and served perfect to you.
-            </h2>
-          </div>
-        </div>
-
-        <div className="md:px-40">
-          <div className="bg-redBg p-10 md:rounded-lg shadow-lg ">
-            <div className="pb-4 md:w-1/2 text-center">
-              <h1 className="font-nmr text-4xl font-bold text-nmr-lightBrown">
-                Thinking of Catering?
+            <div className="max-w-xs">
+              <h1 className="font-nmr text-3xl sm:text-2xl lg:text-3xl text-nmr-redTxt">
+                Fresh Ingredients
               </h1>
+              <h2 className="font-semibold">
+                We use only the best ingrdients, making sure nothing is ever
+                frozen and served perfect to you.
+              </h2>
             </div>
-            <form className="md:w-1/2 text-center">
-              <div className="w-full space-y-5 p-2">
-                <input
-                  placeholder="name"
-                  className="bg-nmr-lightBrown px-5 py-2 rounded-md w-full "
-                ></input>
-                <input
-                  placeholder="email address"
-                  className="bg-nmr-lightBrown px-5 py-2 rounded-md w-full "
-                ></input>
-                <input
-                  placeholder="date of the event"
-                  className="bg-nmr-lightBrown px-5 py-2 rounded-md w-full "
-                ></input>
-                <input
-                  placeholder="number of people approx"
-                  className="bg-nmr-lightBrown px-5 py-2 rounded-md w-full "
-                ></input>
-                <input
-                  placeholder="details about the event (time, type of venue, private party or company event, etc.)"
-                  className="bg-nmr-lightBrown px-5 py-2 rounded-md w-full "
-                ></input>
-                <button className="bg-nmr-black text-nmr-lightBrown font-nmr px-10 py-3 text-xl rounded-md">
-                  Send
-                </button>
-              </div>
-            </form>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="pb-4">
+              <Image src="/phone.png" alt="phone icon" width={59} height={59} />
+            </div>
+            <div className=" max-w-xs">
+              <h1 className="font-nmr text-3xl sm:text-2xl lg:text-3xl text-nmr-redTxt">
+                Fresh Ingredients
+              </h1>
+              <h2 className="font-semibold">
+                We use only the best ingrdients, making sure nothing is ever
+                frozen and served perfect to you.
+              </h2>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="pb-4">
+              <Image src="/phone.png" alt="phone icon" width={59} height={59} />
+            </div>
+            <div className=" max-w-xs">
+              <h1 className="font-nmr text-3xl sm:text-2xl lg:text-3xl text-nmr-redTxt">
+                Fresh Ingredients
+              </h1>
+              <h2 className="font-semibold">
+                We use only the best ingrdients, making sure nothing is ever
+                frozen and served perfect to you.
+              </h2>
+            </div>
           </div>
         </div>
+        <Form />
       </div>
     </div>
   );
