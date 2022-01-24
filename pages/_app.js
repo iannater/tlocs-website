c62b9dba24a5915b36e05a6b883cd0c714d94546
Fabian <acostahf4@gmail.com>
2022-01-24 15:47:19 -0600

    working on metadata

--- pages/_app.js
+++ pages/_app.js
@@ -11,13 +11,36 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <title>T-loc</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>T-locs</title>
+        <meta name="title" content="T-locs" />
         <meta
           name="description"
           content="T-locs website description goes here"
         />
-        <meta property="og:images" content="/thumbnail.png" />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://t-loc-s-website.vercel.app/" />
+        <meta property="og:title" content="T-locs" />
+        <meta
+          property="og:description"
+          content="T-locs website description goes here"
+        />
+        <meta property="og:image" content="/thumbnail.png" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://t-loc-s-website.vercel.app/"
+        />
+        <meta property="twitter:title" content="T-locs" />
+        <meta
+          property="twitter:description"
+          content="T-locs website description goes here"
+        />
+        <meta property="twitter:image" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/blklogo.png" />
         {/* Twitter */}
         {/* <meta name="twitter:card" content="summary" key="twcard" /> */}
