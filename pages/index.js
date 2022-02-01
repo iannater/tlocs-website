17996c805f8fcca3a2b4e95ae9435a64c230895e
Fabian <acostahf4@gmail.com>
2022-02-01 17:48:19 -0600

    add hook for gsap

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,20 @@ import { gql } from "@apollo/client";
 import client from "../apolloClient";
 import HomeHero from "../components/homeHero";
 import Layout from "../components/layout";
+import { useEffect } from "react";
+import { gsap } from "gsap/dist/gsap";
+import Accordion from "../components/accordion";
 
 export default function Home({ menus }) {
+  useEffect(() => {
+    var tl = gsap.timeline();
+    tl.from("#element", { duration: 0.5, y: -100, opacity: 0 });
+    tl.from("#heading", { duration: 0.5, y: -100, opacity: 0 });
+  }, []);
   return (
     <div>
       <HomeHero />
+      <Accordion {...menus} />
       <div className="bg-yellowBg bg-no-repeat bg-cover min-h-screen">
         <Hotdog />
         <Menu {...menus} />
