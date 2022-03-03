5b5e668b0ef8176e0dacb4edf436d1078387d1ac
Fabian <acostahf4@gmail.com>
2022-03-02 18:24:51 -0600

    fix mobile height

--- components/homeHero.js
+++ components/homeHero.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import { BsChevronDoubleDown } from "@react-icons/all-files/bs/BsChevronDoubleDown";
 
 const homeHero = () => {
+  useEffect(() => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+
   return (
-    <div className="inset-0 h-screen bg-redBg bg-cover bg-no-repeat flex flex-col justify-center md:justify-start  items-center">
+    <div className="mobile h-screen bg-redBg bg-cover bg-no-repeat flex flex-col justify-center md:justify-start  items-center">
       <div className="w-full flex flex-col items-center font-nmr font-bold md:absolute md:top-28  z-10 ">
         <h3 id="heading" className="text-nmr-lightBrown md:text-xl ">
           T-Loc&apos;s World Famous
@@ -37,6 +44,7 @@ const homeHero = () => {
         <Image
           src="/images/home-hotdog.png"
           layout="responsive"
+          priority
           width={1235}
           height={507}
           alt="hot dog"
