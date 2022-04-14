64d8cca4419c95a20cd19f3de30c1e6b8c2c1572
Fabian <acostahf4@gmail.com>
2022-04-13 19:09:52 -0500

    new email and hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -1,65 +1,71 @@
 import "../styles/globals.css";
 import Layout1 from "../components/layout";
 import Layout2 from "../components/layoutSd";
-
+import { hotjar } from "react-hotjar";
+import { useEffect } from "react";
 import React from "react";
 import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-  const Layout = Component.Layout ? Component.Layout : React.Fragment;
+	const Layout = Component.Layout ? Component.Layout : React.Fragment;
+	useEffect(() => {
+		hotjar.initialize(
+			process.env.NEXT_PUBLIC_hjid,
+			process.env.NEXT_PUBLIC_hjsfv
+		);
+	}, []);
+	return (
+		<Layout>
+			<Head>
+				<title>T-locs</title>
+				<meta name="title" content="T-locs" />
+				<meta
+					name="description"
+					content="T-locs website description goes here"
+				/>
 
-  return (
-    <Layout>
-      <Head>
-        <title>T-locs</title>
-        <meta name="title" content="T-locs" />
-        <meta
-          name="description"
-          content="T-locs website description goes here"
-        />
+				{/* <!-- Open Graph / Facebook --> */}
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://t-loc-s-website.vercel.app/" />
+				<meta property="og:title" content="T-locs" />
+				<meta
+					property="og:description"
+					content="T-locs website description goes here"
+				/>
+				<meta
+					property="og:image"
+					content={`https://t-loc-s-website.vercel.app/thumbnail.png`}
+				/>
 
-        {/* <!-- Open Graph / Facebook --> */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://t-loc-s-website.vercel.app/" />
-        <meta property="og:title" content="T-locs" />
-        <meta
-          property="og:description"
-          content="T-locs website description goes here"
-        />
-        <meta
-          property="og:image"
-          content={`https://t-loc-s-website.vercel.app/thumbnail.png`}
-        />
+				{/* <!-- Twitter --> */}
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta
+					property="twitter:url"
+					content="https://t-loc-s-website.vercel.app/"
+				/>
+				<meta property="twitter:title" content="T-locs" />
+				<meta
+					property="twitter:description"
+					content="T-locs website description goes here"
+				/>
+				<meta
+					property="twitter:image"
+					content={`https://t-loc-s-website.vercel.app/thumbnail.png`}
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/blklogo.png" />
+				{/* Twitter */}
+				{/* <meta name="twitter:card" content="summary" key="twcard" /> */}
+				{/* <meta name="twitter:creator" content={twitterHandle} key="twhandle" /> */}
 
-        {/* <!-- Twitter --> */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:url"
-          content="https://t-loc-s-website.vercel.app/"
-        />
-        <meta property="twitter:title" content="T-locs" />
-        <meta
-          property="twitter:description"
-          content="T-locs website description goes here"
-        />
-        <meta
-          property="twitter:image"
-          content={`https://t-loc-s-website.vercel.app/thumbnail.png`}
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/blklogo.png" />
-        {/* Twitter */}
-        {/* <meta name="twitter:card" content="summary" key="twcard" /> */}
-        {/* <meta name="twitter:creator" content={twitterHandle} key="twhandle" /> */}
-
-        {/* Open Graph */}
-        {/* <meta property="og:url" content={currentURL} key="ogurl" />
+				{/* Open Graph */}
+				{/* <meta property="og:url" content={currentURL} key="ogurl" />
           <meta property="og:image" content={previewImage} key="ogimage" />
           <meta property="og:site_name" content={siteName} key="ogsitename" />
           <meta property="og:title" content={pageTitle} key="ogtitle" />
           <meta property="og:description" content={description} key="ogdesc" /> */}
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
-  );
+			</Head>
+			<Component {...pageProps} />
+		</Layout>
+	);
 }
