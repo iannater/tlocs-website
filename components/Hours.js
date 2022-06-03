6fc0fae8de7343709bc9b2d162e88e1f96d0315d
Ian_Nater <ian@7pi.co>
2022-06-03 14:42:00 -0500

    testing a change to hours

--- components/Hours.js
+++ components/Hours.js
@@ -11,12 +11,12 @@ const Hours = (props) => {
         <div className="grid grid-cols-2 text-center text-nmr-lightBrown md:text-2xl ">
           <div>
             <div className="pb-5">
-              <h1 className="font-bold"> {props.hours[0].day} </h1>
-              <h1 className="font-medium">{props.hours[0].hours}</h1>
+              <h1 className="font-bold"> {props.hours[1].day} </h1>
+              <h1 className="font-medium">{props.hours[1].hours}</h1>
             </div>
             <div className="pb-5">
-              <h1 className="font-bold"> {props.hours[1].day} </h1>
-              <h1 className="font-medium"> {props.hours[1].hours} </h1>
+              <h1 className="font-bold"> {props.hours[0].day} </h1>
+              <h1 className="font-medium"> {props.hours[0].hours} </h1>
             </div>
             <div className="pb-5">
               <h1 className="font-bold"> {props.hours[2].day}</h1>
